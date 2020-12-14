78e4b0963aeedd157f687d78905b453b922641e2
mrbasit <itechnician030@gmail.com>
2020-12-14 13:24:19 +0500

    login-signup improved

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 // Home Nav
-const headerNavToggle = document.querySelector('.header-nav-toggle');
-const headerNav = document.querySelector('.main_nav');
-const headerLogo = document.querySelector('.logo img');
-let navOpen = false;
-headerNavToggle.addEventListener('click',headerNavFunc);
-function headerNavFunc(){
-    headerNav.classList.toggle('active');
-        if(!navOpen){
+try {
+    const headerNavToggle = document.querySelector('.header-nav-toggle');
+    const headerNav = document.querySelector('.main_nav');
+    const headerLogo = document.querySelector('.logo img');
+    let navOpen = false;
+    headerNavToggle.addEventListener('click', headerNavFunc);
+    function headerNavFunc() {
+        headerNav.classList.toggle('active');
+        if (!navOpen) {
             navOpen = true;
             setTimeout(() => {
                 headerLogo.src = './img/logo-white.png';
-            }, 500);  
-        }else{
+            }, 500);
+        } else {
             navOpen = false;
             setTimeout(() => {
                 headerLogo.src = './img/logo.png';
             }, 1400);
         }
+    }
+
+} catch (err) {
+    console.log(err);
 }
-// header
+
+// header scroll Func
 try {
-    // Home Page Header
     document.addEventListener("scroll", () => {
         if (scrollY > 100) {
             document.querySelector("header").classList.add("header_shadow");
@@ -34,10 +39,31 @@ try {
 } catch (err) {
     console.log(err);
 }
-// deactivate menu effect
+// deactivate menu effect Func
 try {
     var headerListLinks = document.querySelectorAll('.header_list_2_link');
-    headerListLinks.forEach(item => { item.addEventListener('click',headerNavFunc) });
+    headerListLinks.forEach(item => { item.addEventListener('click', headerNavFunc) });
+} catch (err) {
+    console.log(err);
+}
+
+// form Func
+try {
+    const inputs = document.querySelectorAll('.input');
+    function focusFunc() {
+        let parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+    }
+    function blurFunc() {
+        let parent = this.parentNode.parentNode;
+        if (this.value == "") {
+            parent.classList.remove('focus');
+        }
+    }
+    inputs.forEach(input => {
+        input.addEventListener('focus', focusFunc);
+        input.addEventListener('blur', blurFunc);
+    });
 } catch (err) {
     console.log(err);
 }
